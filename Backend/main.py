cbd96e382d89aaeba2132fc2536aec4fe04b27c9
Shibam802 <shibambanik0@gmail.com>
2025-08-27 01:40:56 +0530

    Fix: reduced memory usage and switched to smaller model

--- Backend/main.py
+++ Backend/main.py
@@ -18,7 +18,7 @@ app = FastAPI()
 # Allow frontend to connect
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # change to frontend URL in production
+    allow_origins=["https://legal-sentiment-analysis-website.vercel.app"],  # change to frontend URL in production
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
